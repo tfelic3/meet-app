e2eb6523f592f012615b23f6e2f7de5f5b5ba58e
tfelic3 <felicien.tesalon@gmail.com>
2021-11-21 22:55:06 -0500

    App not displaying locally
    Local storage is cached but not showing.

--- src/App.js
+++ src/App.js
@@ -1,71 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import EventList from './EventList.js';
-import CitySearch from './CitySearch';
-import NumberOfEvents from './NumberOfEvents';
-import {getEvents} from './api.js';
-import "./nprogress.css";
-import {extractLocations} from "./api.js";
 
-
-class App extends Component {
-
-	
-	state = {
-		events: [],
-		locations: [],
-		NumberOfEvents: 32,
-	};
-
-	
-	  updateEvents = (location) => {
-		getEvents().then((response) => {
-		  const locationEvents = (location === 'all') ?
-			response.events :
-			response.events.filter((event) => event.location === location);
-		  this.setState({
-			events: locationEvents,
-			locations: response.locations
-			
-		  });
-		});
-	  }
-
-	  componentDidMount() {
-		
-		this.mounted = true;
-		getEvents().then((response) => {
-		
-		
-		  if(this.mounted) {
-		  this.setState({ events: response.events, locations: response.locations,});
-	
-		  }
-		});
-	  }
-	
-	  componentWillUnmount(){
-		this.mounted = false;
-	  }
-
-  
-	 
-	
-
-	render() {
-		return (
-			<div className="App">
-				<p>City Search Bar</p>
-				
-				<CitySearch
-					locations={this.state.locations}
-					updateEvents={this.updateEvents}
-				/>
-				<NumberOfEvents updateEvents={this.updateEvents} />
-				<EventList events={this.state.events} />
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
